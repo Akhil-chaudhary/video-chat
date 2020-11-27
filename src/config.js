4f088638cbaa1e7e88b4c2c52ff4c26a831b5dd4
Akhil-chaudhary <91989976a@gmail.com>
2020-11-27 21:12:26 +0530

    Initial Commit

---
+++ src/config.js
@@ -0,0 +1,12 @@
+const config = {
+  apiKey: "AIzaSyD_PJQQA2-1OTuYay0eGSHtLjQ-MYflqFk",
+  authDomain: "codebuggedakhil.firebaseapp.com",
+  databaseURL: "https://codebuggedakhil.firebaseio.com",
+  projectId: "codebuggedakhil",
+  storageBucket: "codebuggedakhil.appspot.com",
+  messagingSenderId: "769327321662",
+  appId: "1:769327321662:web:ec00e26b39e6eaa493776f",
+  measurementId: "G-TT1RC7HLCB"
+}
+
+export default config
